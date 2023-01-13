f93fd6686c2072fa4d4e4c1d0fc103b32e0f6b2c
Hyeongmin Park <thalsal@naver.com>
2023-01-13 19:00:32 +0900

    chore : HibernateRepository 라이브러리 삭제

--- api/src/main/java/com/tht/api/app/repository/meta/InterestRepository.java
+++ api/src/main/java/com/tht/api/app/repository/meta/InterestRepository.java
@@ -2,11 +2,10 @@ package com.tht.api.app.repository.meta;
 
 import com.tht.api.app.entity.meta.Interest;
 import com.tht.api.app.facade.response.InterestResponse;
-import io.hypersistence.utils.spring.repository.HibernateRepository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InterestRepository extends HibernateRepository<Interest>, JpaRepository<Interest, Integer> {
+public interface InterestRepository extends JpaRepository<Interest, Integer> {
 
     List<InterestResponse> findAllBy();
 }
