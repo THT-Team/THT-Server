1eb001ea7baa4d8276e0c9c13025e970761b15c3
Hyeongmin Park <thalsal@naver.com>
2023-02-14 10:16:01 +0900

    ✨restdocs 화면 home 으로 설정

--- api/src/main/java/com/tht/api/app/controller/restdocs/RestDocsController.java
+++ api/src/main/java/com/tht/api/app/controller/restdocs/RestDocsController.java
@@ -1,15 +1,14 @@
 package com.tht.api.app.controller.restdocs;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RestDocsController {
 
-    @GetMapping("/rest-docs/{text}")
-    public ResponseEntity<String> getRestDocsHello(@PathVariable(name = "text") final String  text) {
-        return ResponseEntity.ok("Let's Start Spring Rest Docs - " + text);
+    @GetMapping("/")
+    public String getRestDocsHello() {
+        return "docs/index.html";
     }
+
 }
