9d5b5873ab8cd683343b3dff3f5d3155d52004fd
Hyeongmin Park <thalsal@naver.com>
2023-03-09 20:48:37 +0900

    📌 난수 생성 버그 fix

--- api/src/test/java/com/tht/api/app/config/RandomUtilsTest.java
+++ api/src/test/java/com/tht/api/app/config/RandomUtilsTest.java
@@ -4,12 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.anyInt;
 import static org.mockito.BDDMockito.mock;
 import static org.mockito.BDDMockito.when;
-import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 
 class RandomUtilsTest {
 
@@ -19,7 +17,7 @@ class RandomUtilsTest {
 
         int digits = 3;
         int number = RandomUtils.getInstance().getNumberOfDigits(digits);
-        assertThat(number).isBetween((int) Math.pow(10, 2) , (int) Math.pow(10, 3));
+        assertThat(number).isBetween(0, (int) Math.pow(10, 3));
     }
 
     @Test
