b8e6d7d654bc28c61fd6ca199ca616b2c6cbe75e
Park Hyeongmin <thalsal@naver.com>
2023-04-14 13:50:57 +0900

    THT-Server-33 Restdocs + Swagger UI 설정 (#34)
    * chore : 의존성 및 build task 설정
    
    * chore : 주석 제거
    
    * chore : swagger-ui 경로 설정
    
    * fix : 응답값 양식 변경
    
    * update : gitignore
    
    * feat : swagger-ui 적용

--- api/src/test/java/com/tht/api/app/controller/ControllerTestConfig.java
+++ api/src/test/java/com/tht/api/app/controller/ControllerTestConfig.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +16,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @ExtendWith({RestDocumentationExtension.class})
 public abstract class ControllerTestConfig {
 
--- api/src/test/java/com/tht/api/app/controller/InterestControllerTest.java
+++ api/src/test/java/com/tht/api/app/controller/InterestControllerTest.java
@@ -1,13 +1,23 @@
 package com.tht.api.app.controller;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
 import com.tht.api.app.facade.interest.InterestFacade;
-import java.net.URI;
+import com.tht.api.app.facade.interest.response.InterestResponse;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,12 +34,34 @@ class InterestControllerTest extends ControllerTestConfig {
     @DisplayName("관심사 목록 리스트 조회")
     void getInterestAll() throws Exception {
 
+        //given
+        when(interestFacade.getInterestList()).thenReturn(
+            List.of(new InterestResponse(1, "이상형 명칭", "emoji code"))
+        );
+
+        //then
         ResultActions resultActions = mockMvc.perform(
-            RestDocumentationRequestBuilders.get(URI.create(DEFAULT_URL))
+            RestDocumentationRequestBuilders.get(DEFAULT_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-         ).andDo(
-            MockMvcRestDocumentation.document("interest-docs")
+        ).andDo(
+            MockMvcRestDocumentationWrapper.document("interest-docs",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .tag("관심사 목록")
+                        .description("관심사 목록 리스트 조회")
+                        .requestFields()
+                        .responseFields(
+                            fieldWithPath("[].idx").description("idx"),
+                            fieldWithPath("[].name").description("이상형 명칭"),
+                            fieldWithPath("[].emojiCode").description("이모티콘 코드")
+                        )
+                        .responseSchema(Schema.schema("InterestResponse"))
+                        .build()
+                )
+            )
         );
 
         resultActions.andExpect(MockMvcResultMatchers.status().isOk());
