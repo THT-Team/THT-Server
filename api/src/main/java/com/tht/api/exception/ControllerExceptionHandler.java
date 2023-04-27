0b10eab6909daacc8429f1021d2dba7c99ee8d8c
Hyeongmin Park <thalsal@naver.com>
2023-04-27 14:02:19 +0900

    add: entity state 공통 exception 처리

--- api/src/main/java/com/tht/api/exception/ControllerExceptionHandler.java
+++ api/src/main/java/com/tht/api/exception/ControllerExceptionHandler.java
@@ -3,8 +3,12 @@ package com.tht.api.exception;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import com.tht.api.exception.custom.AligoException;
+import com.tht.api.exception.custom.EntityStateException;
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.format.DateTimeParseException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,5 +25,38 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ErrorResponse> handlerException(final AligoException e, final HttpServletRequest request) {
         return ResponseEntity.badRequest()
             .body(ErrorResponse.of(BAD_REQUEST, e.getMessage(), request));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(final MethodArgumentNotValidException e, final HttpServletRequest request) {
+
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.of(BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(), request)
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(final DateTimeParseException e, final HttpServletRequest request) {
+
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.of(BAD_REQUEST,e.getMessage(), request)
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(final HttpMessageNotReadableException e, final HttpServletRequest request) {
+
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.of(BAD_REQUEST,e.getMessage(), request)
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handlerException(final EntityStateException e, final HttpServletRequest request) {
+
+        return ResponseEntity.badRequest().body(
+            ErrorResponse.of(BAD_REQUEST,e.getMessage(), request)
+        );
     }
 }
