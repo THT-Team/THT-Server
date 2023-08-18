0919f97be6b6ce3bd6c69203fc74ed84edef78de
Hyeongmin Park <thalsal@naver.com>
2023-08-18 11:03:22 +0900

    fix : jenkins ci 시 PortInUseException 에러 해결

--- api/src/test/java/com/tht/api/app/acceptance/config/AcceptanceTest.java
+++ api/src/test/java/com/tht/api/app/acceptance/config/AcceptanceTest.java
@@ -4,11 +4,12 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
 
     @Autowired
