d071d72fb202915386b5f6b94739b6e8d1060ede
Hyeongmin Park <thalsal@naver.com>
2023-08-29 11:57:19 +0900

    refactor : 유저 생성 인수테스트 공통부분 부모 객체로 추출

--- api/src/test/java/com/tht/api/app/acceptance/config/AcceptanceTest.java
+++ api/src/test/java/com/tht/api/app/acceptance/config/AcceptanceTest.java
@@ -1,6 +1,16 @@
 package com.tht.api.app.acceptance.config;
 
+import com.tht.api.app.config.security.TokenProvider;
+import com.tht.api.app.entity.meta.DailyFalling;
+import com.tht.api.app.entity.meta.DailyFallingActiveTimeTable;
+import com.tht.api.app.entity.meta.IdealType;
+import com.tht.api.app.entity.meta.Interest;
+import com.tht.api.app.repository.meta.DailyFallingActiveTimeTableRepository;
+import com.tht.api.app.repository.meta.DailyFallingRepository;
+import com.tht.api.app.repository.meta.IdealTypeRepository;
+import com.tht.api.app.repository.meta.InterestRepository;
 import io.restassured.RestAssured;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +25,21 @@ public abstract class AcceptanceTest {
     @Autowired
     private DatabaseCleanup databaseCleanup;
 
+    @Autowired
+    DailyFallingRepository dailyFallingRepository;
+
+    @Autowired
+    DailyFallingActiveTimeTableRepository dailyFallingActiveTimeTableRepository;
+
+    @Autowired
+    InterestRepository interestRepository;
+
+    @Autowired
+    IdealTypeRepository idealTypeRepository;
+
+    @Autowired
+    TokenProvider tokenProvider;
+
     @LocalServerPort
     private int port;
 
@@ -22,5 +47,27 @@ public abstract class AcceptanceTest {
     public void setUp() {
         RestAssured.port = port;
         databaseCleanup.execute();
+
+        setUpCreateUser();
+    }
+
+    private void setUpCreateUser() {
+        for(int i=0; i<3; i++){
+            interestRepository.save(Interest.of("관심사"+i,""));
+            idealTypeRepository.save(IdealType.of("이상형"+i, ""));
+        }
+    }
+
+    public DailyFalling 그날의주제어_생성_요청() {
+        LocalDateTime now = LocalDateTime.now();
+
+        DailyFallingActiveTimeTable timeTable = dailyFallingActiveTimeTableRepository.save(
+            DailyFallingActiveTimeTable.of(now.minusDays(1), now.plusDays(1)));
+
+        return dailyFallingRepository.save(DailyFalling.of(1, timeTable.getIdx(), "주제어"));
+    }
+
+    public String getUserUuid(String accessToken) {
+        return tokenProvider.getAuthentication(accessToken).getCredentials().toString();
     }
 }
