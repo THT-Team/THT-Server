71acc104d68f655039eb31fa8d338bf45434d31d
Hyeongmin Park <thalsal@naver.com>
2023-11-30 14:10:08 +0900

    refactor : jwt token 발급 비지니스 로직 리팩토링

--- api/src/main/java/com/tht/api/app/config/security/filter/JwtFilter.java
+++ api/src/main/java/com/tht/api/app/config/security/filter/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.tht.api.app.config.security.filter;
 
+import com.tht.api.app.config.security.SecurityConst;
 import com.tht.api.app.config.security.TokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
@@ -24,7 +24,7 @@ public class JwtFilter extends OncePerRequestFilter  {
         final HttpServletResponse response, final FilterChain filterChain)
         throws ServletException, IOException {
 
-        final String token = getParseJwt(request.getHeader("Authorization"));
+        final String token = tokenProvider.getParseJwt(request.getHeader(SecurityConst.AUTH_HEADER_NAME));
 
         if (token != null && tokenProvider.validateToken(token)) {
             // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
@@ -34,14 +34,5 @@ public class JwtFilter extends OncePerRequestFilter  {
 
         filterChain.doFilter(request, response);
 
-    }
-
-    private String getParseJwt(final String headerAuth) {
-
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")) {
-            return headerAuth.substring(7);
-        }
-
-        return null;
     }
 }
