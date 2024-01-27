4b8fcc48f0ac358e20a5a5765126a597cc8f0917
Park Hyeongmin <thalsal@naver.com>
2024-01-27 09:21:47 +0900

    Tht server 175 그날의 토픽 주제 테스트 메크로 (#176)
    * chore : add spring scheduled
    
    * docs : 불필요한 주석 삭제
    
    * feat : 데일리 토픽 주제 더미 데이터 생성 매크로

---
+++ api/src/main/java/com/tht/api/app/config/ScheduledConfig.java
@@ -0,0 +1,11 @@
+package com.tht.api.app.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@EnableScheduling
+@Component
+public class ScheduledConfig {
+}
