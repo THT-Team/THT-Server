c74287fa2a26443243377bfdecf6118f75843f0d
thalals <thalsal@naver.com>
2024-01-29 17:33:26 +0900

    fix : 데일리 토픽 주제 시차 계산하여 수정

--- api/src/test/java/com/tht/api/app/InsertDailyFallingDummyData.java
+++ api/src/test/java/com/tht/api/app/InsertDailyFallingDummyData.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @SpringBootTest
@@ -26,19 +26,23 @@ class InsertDailyFallingDummyData {
     @Test
     public void insertDailyFallingTalkSubjectData() {
 
-        //insert time table
-        //가장 최근 시간 조회 + 10분
-        DailyFallingActiveTimeTable topTimeTable = timeTableRepository.findTopByOrderByEndDateTimeDesc();
-        DailyFallingActiveTimeTable save = timeTableRepository.save(DailyFallingActiveTimeTable.of(topTimeTable.getEndDateTime(), topTimeTable.getEndDateTime().plusMinutes(10)));
+        for(int i=0;i<6*24*3;i++) {
+            //insert time table
+            //가장 최근 시간 조회 + 10분
+            DailyFallingActiveTimeTable topTimeTable = timeTableRepository.findTopByOrderByEndDateTimeDesc();
+            DailyFallingActiveTimeTable save = timeTableRepository.save(DailyFallingActiveTimeTable.of(topTimeTable.getEndDateTime(), topTimeTable.getEndDateTime().plusMinutes(10)));
 
-        //insert daily falling
-        dailyFallingRepository.saveAll(
-                List.of(
-                        DailyFalling.of(1, save.getIdx(), "토픽주제 매크로 1 ["+ LocalDateTime.now()+"]"),
-                        DailyFalling.of(2, save.getIdx(), "토픽주제 매크로 2 ["+LocalDateTime.now().toString()+"]"),
-                        DailyFalling.of(3, save.getIdx(), "토픽주제 매크로 3 ["+LocalDateTime.now().toString()+"]"),
-                        DailyFalling.of(4, save.getIdx(), "토픽주제 매크로 4 ["+LocalDateTime.now().toString()+"]")
-                )
-        );
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            //insert daily falling
+            dailyFallingRepository.saveAll(
+                    List.of(
+                            DailyFalling.of(1, save.getIdx(), "토픽주제 1 [" + save.getStartDateTime().minusHours(9).format(formatter) + " ~ " + save.getEndDateTime().minusHours(9).format(formatter) + "]"),
+                            DailyFalling.of(2, save.getIdx(), "토픽주제 2 [" + save.getStartDateTime().minusHours(9).format(formatter) + " ~ " + save.getEndDateTime().minusHours(9).format(formatter) + "]"),
+                            DailyFalling.of(3, save.getIdx(), "토픽주제 3 [" + save.getStartDateTime().minusHours(9).format(formatter) + " ~ " + save.getEndDateTime().minusHours(9).format(formatter) + "]"),
+                            DailyFalling.of(4, save.getIdx(), "토픽주제 4 [" + save.getStartDateTime().minusHours(9).format(formatter) + " ~ " + save.getEndDateTime().minusHours(9).format(formatter) + "]")
+                    )
+            );
+        }
     }
 }
