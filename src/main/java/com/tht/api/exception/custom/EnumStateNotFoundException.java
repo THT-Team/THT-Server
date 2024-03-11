50854ec60139e8a2e6a8c6d903ea835522f3d16f
thalals <thalsal@naver.com>
2024-03-11 15:47:54 +0900

    add : UserFrequencyConverter Exception class

--- src/main/java/com/tht/api/exception/custom/EnumStateNotFoundException.java
+++ src/main/java/com/tht/api/exception/custom/EnumStateNotFoundException.java
@@ -30,4 +30,8 @@ public class EnumStateNotFoundException extends RuntimeException {
         return new EnumStateNotFoundException("SNS 통합회원가입은 NORMAL 을 제외한 유효한 타입만 가능합니다.");
     }
 
+    public static EnumStateNotFoundException ofUserFrequency(final String findUserFrequency) {
+        return new EnumStateNotFoundException(String.format("%s는 유효하지 않은 User Frequency 타입입니다.", findUserFrequency));
+    }
+
 }
