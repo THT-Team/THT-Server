7a9eac5339e449388e4f543165cf82876d315e3c
thalals <thalsal@naver.com>
2024-07-30 16:55:09 +0900

    docs : 반복되는 페이징 응답값 필드 상위 추상화 클래스로 추출

--- tht-admin/src/test/java/com/tht/thtadmin/docs/UserManageDocs.java
+++ tht-admin/src/test/java/com/tht/thtadmin/docs/UserManageDocs.java
@@ -25,15 +25,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.*;
+import java.util.List;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.tht.thtadmin.docs.config.DocsConst.getPagingFieldDescriptors;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
@@ -176,7 +176,6 @@ class UserManageDocs extends ControllerTestConfig {
                     parameterWithName("direction").optional().ignored().description("정렬 방식")
                 )
                 .responseFields(
-
                     getPagingFieldDescriptors(List.of(
                         fieldWithPath("content[].username").type(JsonFieldType.STRING).description("유저 이름"),
                         fieldWithPath("content[].userUuid").type(JsonFieldType.STRING).description("유저 고유 번호"),
@@ -239,32 +238,6 @@ class UserManageDocs extends ControllerTestConfig {
         resultActions.andExpect(MockMvcResultMatchers.status().isOk());
     }
 
-    private static FieldDescriptor[] getPagingFieldDescriptors(List<FieldDescriptor> apiFieldDescriptors) {
 
-        List<FieldDescriptor> fieldDescriptors = new ArrayList<>(List.of(
-            fieldWithPath("pageable.sort.empty").description("정렬이 비어있는지 여부"),
-            fieldWithPath("pageable.sort.sorted").description("내용이 정렬되었는지 여부"),
-            fieldWithPath("pageable.sort.unsorted").description("내용이 정렬되지 않았는지 여부"),
-            fieldWithPath("pageable.offset").description("현재 페이지의 오프셋"),
-            fieldWithPath("pageable.pageNumber").description("현재 페이지 번호"),
-            fieldWithPath("pageable.pageSize").description("페이지당 항목 수"),
-            fieldWithPath("pageable.paged").description("페이지 매김이 활성화되었는지 여부"),
-            fieldWithPath("pageable.unpaged").description("페이지 매김이 비활성화되었는지 여부"),
-            fieldWithPath("last").description("마지막 페이지인지 여부"),
-            fieldWithPath("totalElements").description("전체 요소 수"),
-            fieldWithPath("totalPages").description("전체 페이지 수"),
-            fieldWithPath("first").description("첫 번째 페이지인지 여부"),
-            fieldWithPath("size").description("페이지 크기"),
-            fieldWithPath("number").description("현재 페이지 번호"),
-            fieldWithPath("sort.empty").description("정렬이 비어있는지 여부"),
-            fieldWithPath("sort.sorted").description("내용이 정렬되었는지 여부"),
-            fieldWithPath("sort.unsorted").description("내용이 정렬되지 않았는지 여부"),
-            fieldWithPath("numberOfElements").description("현재 페이지의 요소 수"),
-            fieldWithPath("empty").description("현재 페이지가 비어 있는지 여부")
-        ));
-
-        fieldDescriptors.addAll(apiFieldDescriptors);
-        return fieldDescriptors.toArray(FieldDescriptor[]::new);
-    }
 
 }
