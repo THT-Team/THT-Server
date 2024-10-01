09cf7b88f5198522f6450c5ec93d013a09b106e0
Park Hyeongmin <thalsal@naver.com>
2024-10-01 15:49:15 +0900

    THT-Server-266 [APP] 탈퇴 유저 신규 가입 후 로그인 에러 처리 (#267)

--- tht-core/domain/src/main/java/com/tht/domain/entity/user/repository/UserRepository.java
+++ tht-core/domain/src/main/java/com/tht/domain/entity/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.tht.domain.entity.user.User;
 import com.tht.domain.entity.user.repository.querydsl.UserCustomRepository;
 import com.tht.enums.EntityState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long>, UserCustomRep
     boolean existsByUsernameAndStateEquals(final String userName, final EntityState state);
     boolean existsByPhoneNumberAndStateEquals(final String phoneNumber, final EntityState state);
 
+    @Query("select u from User u where u.phoneNumber=:phoneNumber and u.state = 'ACTIVE'")
     Optional<User> findByPhoneNumber(final String phoneNumber);
     Optional<User> findByUserUuid(final String userUuid);
 
