1f8f2ac1b162e8bcd6c5bf1e6fea6853865bba46
thalals <thalsal@naver.com>
2025-01-21 23:49:12 +0900

    feat: 개발용 데이터 초기화 api

--- tht-core/domain/src/main/java/com/tht/domain/entity/like/UserLikeRepository.java
+++ tht-core/domain/src/main/java/com/tht/domain/entity/like/UserLikeRepository.java
@@ -2,6 +2,7 @@ package com.tht.domain.entity.like;
 
 import com.tht.enums.user.LikeState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +11,7 @@ public interface UserLikeRepository extends JpaRepository<UserLike, Long>,
 
     Optional<UserLike> findByUserUuidAndTargetUserUuidAndDailyFallingIdxAndLikeState(
         String userUuid, String targetUserUuid, long dailyFallingIdx, LikeState likeState);
+
+    @Transactional
+    void deleteAllByUserUuid(String userUuid);
 }
--- tht-core/domain/src/main/java/com/tht/domain/entity/user/repository/UserDailyFallingRepository.java
+++ tht-core/domain/src/main/java/com/tht/domain/entity/user/repository/UserDailyFallingRepository.java
@@ -3,8 +3,11 @@ package com.tht.domain.entity.user.repository;
 import com.tht.domain.entity.dailyfalling.UserDailyFalling;
 import com.tht.domain.entity.user.repository.querydsl.UserDailyFallingCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserDailyFallingRepository extends JpaRepository<UserDailyFalling, Long>,
     UserDailyFallingCustomRepository {
 
+    @Transactional
+    void deleteAllByUserUuid(String userUuid);
 }
